30d677da84d7ad4c2b701e4b72e1001d9041b9df
MattGelfand <matthew.gelfand@gmail.com>
2013-10-31 15:34:39 -0500

    put css and js into separate files

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$('body').mousemove(function(e){
+$('.blimp').css({
+	'top': e.clientY - 20, 
+	'left': e.clientX - 20});
+});
+$('body').click(function(e){
+	$('.fire').css({
+		'display': 'block',
+		'top': e.clientY + 20,
+		'left': e.clientX - 60
+	}).animate({
+		'top': e.clientY + 150,
+		'left': e.clientY - 100
+	}).fadeOut('fast')
+})
+})

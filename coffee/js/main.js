1824b9d3e293cc32f89386f7405ee7fe12cf186f
MattGelfand <matthew.gelfand@gmail.com>
2014-04-18 16:56:18 -0500

    add untracked files

---
+++ coffee/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.banner').unslider({
+	speed: 700,                
+	delay: 5000,               
+	complete: function() {},   
+	keys: true,                
+	dots: true,              
+	fluid: true              
+});
+});
